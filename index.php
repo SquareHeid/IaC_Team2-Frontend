4506c262a45b53b4d508fb97252307b522bd7bd1
SquareHeid <99138465+SquareHeid@users.noreply.github.com>
2024-10-16 13:46:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -17,9 +17,11 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "<table>";
-    echo "<tr><th>Name</th><th>Roll Number</th></tr>";
-    echo "<tr><td>" . $row["firstname"]. "</td><td>" . $row["rollnumber"]. "</td></tr>";
+    echo <h1>Team 2 Demonstration App</h1>
+    echo <h2> Uses a PHP front end to query a MySQL backend database, deployed via Openshift and with source code controlled via github VCS.
+    echo "<table style="border:2px solid black">";
+    echo "<tr style='border:1px solid black; background-color: grey'><th>Name</th><th>Roll Number</th></tr>";
+    echo "<tr style='border:1px solid black; background-color: light-grey'><td>" . $row["firstname"]. "</td><td>" . $row["rollnumber"]. "</td></tr>";
     echo "<table>";
   }
 } else {
