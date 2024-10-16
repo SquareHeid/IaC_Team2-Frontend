6ae77ca63e6f4484f22bf30858dc4d86d34ea1a7
SquareHeid <99138465+SquareHeid@users.noreply.github.com>
2024-10-16 14:10:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,11 +14,12 @@ if ($conn->connect_error) {
 $sql = "SELECT firstname, rollnumber FROM tb1";
 $result = $conn->query($sql);
 
+echo "<h1>Team 2 Demonstration App</h1>";
+echo "<h2> Uses a PHP front end to query a MySQL backend database, deployed via Openshift and with source code controlled via github VCS.</h2>";
+
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "<h1>Team 2 Demonstration App</h1>";
-    echo "<h2> Uses a PHP front end to query a MySQL backend database, deployed via Openshift and with source code controlled via github VCS.</h2>";
     echo "<table>";
     echo "<tr><th>Name</th><th>Roll Number</th></tr>";
     echo "<tr><td>" . $row["firstname"]. "</td><td>" . $row["rollnumber"]. "</td></tr>";
